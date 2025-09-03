31b22b8329bbb7be723684d975181de5293f0cbe
David Young <davidsamuelyoung@protonmail.com>
2025-09-03 20:02:36 +0200

    feat: enhance data tools with import/export functionality and success messages

--- lib/shared/domain/rainfall_entry.dart
+++ lib/shared/domain/rainfall_entry.dart
@@ -1,4 +1,3 @@
-import "package:cloud_firestore/cloud_firestore.dart";
 import "package:freezed_annotation/freezed_annotation.dart";
 import "package:rain_wise/shared/domain/rain_gauge.dart";
 
@@ -6,26 +5,18 @@ part "rainfall_entry.freezed.dart";
 
 part "rainfall_entry.g.dart";
 
-// Helper for Firestore Timestamps
-class TimestampConverter implements JsonConverter<DateTime, Timestamp> {
-  const TimestampConverter();
-
-  @override
-  DateTime fromJson(final Timestamp timestamp) => timestamp.toDate();
-
-  @override
-  Timestamp toJson(final DateTime date) => Timestamp.fromDate(date);
-}
-
 @freezed
 abstract class RainfallEntry with _$RainfallEntry {
   const factory RainfallEntry({
     required final double amount,
-    @TimestampConverter() required final DateTime date,
+    required final DateTime date,
     required final String gaugeId,
     required final String unit,
     @JsonKey(includeIfNull: false) final String? id,
-    @JsonKey(includeFromJson: false, includeToJson: false)
+    @JsonKey(
+      includeFromJson: false,
+      includeToJson: false,
+    )
     final RainGauge? gauge,
   }) = _RainfallEntry;
 
